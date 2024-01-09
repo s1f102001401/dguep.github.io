8fb12223f87a3d649fe50182dd947ee66ea386c6
s1f102001401 <s1f102001401@iniad.org>
2024-01-09 21:46:44 +0900

    2146

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ $(function(){
     dis *= -1;
   });
 });
+
+$(function(){
+  $("a").click(function(){
+    var dis = $(this).html()-1;
+    $("#sections").animate({"left":dis*-700+"px"},300);
+    $("body").css("background-positions",dis*-20+"px 100%");
+    $("bg1").css("background-positions",dis*-150+"px 100%");
+    $("bg2").css("background-positions",dis*-700+"px 100%");
+    $(".current").removeClass("current");
+    $(this).addClass("current");
+    return false;
+  })
+})
